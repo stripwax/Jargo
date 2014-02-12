13a7ed55df764e9bbeb61c481604b9275b4e8fc0
stripwax <dave@beermex.com>
2014-02-12 00:38:52 +0000

    adding CARGO BOT tutorial levels

--- level_data.js
+++ level_data.js
@@ -1,6 +1,12 @@
+var levels_data_all_categories = [
+  "Very Very Easy",
+  "Not Quite So Easy",
+  "Tutorial (CARGO BOT)",
+];
+
 var levels_data = [
   {
-    rating:"Easy",
+    rating:"Very Very Easy",
     score:3,
     title:"Move it from here to there!",
     initial_state:[
@@ -12,7 +18,7 @@ var levels_data = [
     crane_x:2,
   },
   {
-    rating:"Easy",
+    rating:"Very Very Easy",
     score:3,
     title:"Go there and then move it!",
     initial_state:[
@@ -24,7 +30,7 @@ var levels_data = [
     crane_x:6,
   },
   {
-    rating:"Easy",
+    rating:"Very Very Easy",
     score:5,
     title:"Move two things",
     initial_state:[
@@ -36,7 +42,7 @@ var levels_data = [
     crane_x:0,
   },
   {
-    rating:"Medium",
+    rating:"Not Quite So Easy",
     score:8,
     title:"My First Swap Experiment!",
     initial_state:[
@@ -46,6 +52,81 @@ var levels_data = [
       [], [], [1], [0], [], [], []
     ],
     crane_x:2,
-  }
+  },
+
+  // CARGO BOT OFFICIAL LEVELS
+  {
+    rating:"Tutorial (CARGO BOT)",
+    score:3,
+    title:"Cargo 101",
+    initial_state:[
+      [0], [],
+    ],
+    goal_state:[
+      [], [0],
+    ],
+    crane_x:0,
+  },
+  {
+    rating:"Tutorial (CARGO BOT)",
+    score:5,
+    title:"Transporter",
+    initial_state:[
+      [0], [], [], [],
+    ],
+    goal_state:[
+      [], [], [], [0],
+    ],
+    crane_x:0,
+  },
+  {
+    rating:"Tutorial (CARGO BOT)",
+    score:4,
+    title:"Re-Curses",
+    initial_state:[
+      [0,0,0,0], [],
+    ],
+    goal_state:[
+      [], [0,0,0,0],
+    ],
+    crane_x:0,
+  },
+  {
+    rating:"Tutorial (CARGO BOT)",
+    score:10,
+    title:"Inverter",
+    initial_state:[
+      [3,2,1,0], [],[],[],[],[],
+    ],
+    goal_state:[
+      [], [],[],[],[],[0,1,2,3],
+    ],
+    crane_x:0,
+  },
+  {
+    rating:"Tutorial (CARGO BOT)",
+    score:5,
+    title:"From Beneath",
+    initial_state:[
+      [0,2,2,2,2], [], [],
+    ],
+    goal_state:[
+      [], [2,2,2,2], [0],
+    ],
+    crane_x:0,
+  },
+  {
+    rating:"Tutorial (CARGO BOT)",
+    score:9,
+    title:"Go Left",
+    initial_state:[
+      [], [1,1,1],[2,2,2],[3,3,3],
+    ],
+    goal_state:[
+      [1,1,1], [2,2,2],[3,3,3],[],
+    ],
+    crane_x:0,
+  },
+
 ];   
 
