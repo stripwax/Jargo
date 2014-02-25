01d85e6de59d77468a4cb3392360f8218f3c78a2
Dave Hooper <dave@beermex.com>
2014-02-25 22:59:38 +0000

    Move some constants, and include crane.js before game

---
+++ limits.js
@@ -0,0 +1,3 @@
+var MAX_BOXES_HEIGHT = 7; // how high can you stack a pile (we could make this depend on level data, but has to be at least as big as the tallest pile you need)
+var MAX_BOXES_WIDTH = 8; // how many piles can there be (depends on level data - i.e. has to be at least as big as the size of the level)
+var TYPES_OF_BOXES = 4; // how many different kinds of boxes there are i.e. 0 thru 3
